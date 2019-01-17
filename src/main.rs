92d9f8f57c978c5ae7398acf70f4c1ca79d03413
rchaser53 <tayoshizawa29@gmail.com>
2019-01-17 23:53:54 +0900

    add error handling for batch_get_item

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,65 @@
 extern crate futures;
 extern crate rusoto_core;
-extern crate rusoto_dynamodb;
 extern crate rusoto_credential;
+extern crate rusoto_dynamodb;
 
-use std::fs::File;
-use std::io::Read;
+use std::collections::HashMap;
+use std::default::Default;
+use std::str;
 
+use rusoto_core::credential::EnvironmentProvider;
 use rusoto_core::Region;
-use rusoto_core::credential::{ProfileProvider};
+// use rusoto_core::credential::ProfileProvider;
+use rusoto_core::credential::ProvideAwsCredentials;
 
-use rusoto_s3::{PutObjectRequest, S3Client, S3};
+use futures::Future;
+use rusoto_dynamodb::{
+    AttributeValue, BatchGetItemError, BatchGetItemInput, DynamoDb, DynamoDbClient,
+    KeysAndAttributes,
+};
 
 fn main() {
-  match ProfileProvider::new() {
-    Ok(result) => {
-      println!("{:?}", result);
-    },
-    Err(err) => panic!("{:?}", err)
-  };
+    let _ = EnvironmentProvider::default().credentials().wait();
+    // match ProfileProvider::new() {
+    //   Ok(result) => {
+    //     println!("{:?}", result);
+    //   },
+    //   Err(err) => panic!("{:?}", err)
+    // };
 
-  let region = Region::Custom {
-      name: "ap-northeast-1".to_owned(),
-      endpoint: "s3.ap-northeast-1.amazonaws.com".to_owned(),
-  };
-  let client = S3Client::new(region.clone());
+    let client = DynamoDbClient::new(Region::ApNortheast1);
 
-    let mut f = File::open("README.md").unwrap();
-    let mut contents: Vec<u8> = Vec::new();
-    match f.read_to_end(&mut contents) {
-        Err(why) => panic!("Error opening file to send to S3: {}", why),
-        Ok(_) => {
-            let req = PutObjectRequest {
-                bucket: "rchaser53-testbacket".to_owned(),
-                key: "nyan".to_owned(),
-                body: Some(contents.into()),
-                ..Default::default()
-            };
-            let result = client.put_object(req).sync().expect("Couldn't PUT object");
-            println!("{:#?}", result);
+    let mut request_items: HashMap<String, KeysAndAttributes> = HashMap::new();
+    let mut attribute_value: HashMap<String, AttributeValue> = HashMap::new();
+    attribute_value.insert(
+        String::from("id"),
+        AttributeValue {
+            s: Some(String::from("test")),
+            ..Default::default()
+        },
+    );
+
+    request_items.insert(
+        String::from("test_dynamo"),
+        KeysAndAttributes {
+            keys: vec![attribute_value],
+            ..Default::default()
+        },
+    );
+
+    match client
+        .batch_get_item(BatchGetItemInput {
+            request_items,
+            return_consumed_capacity: None,
+        })
+        .sync()
+    {
+        Ok(output) => {
+            println!("{:?}", output);
         }
-    }
+        Err(BatchGetItemError::Unknown(msg)) => {
+            println!("Error: {:?}", str::from_utf8(&msg.body).unwrap());
+        }
+        _ => unimplemented!(),
+    };
 }
